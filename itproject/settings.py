449690b85a8be1b5e572f70c336ee6b4a5759fb2
Abu Sufian <abu7suffian@gmail.com>
2022-09-23 17:51:30 +0600

    robots.txt

--- itproject/settings.py
+++ itproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # 'jazzmin',
+    # 'robots',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +46,6 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'mainapp',
     'digitalmarketing',
-    # 'robots',
 ]
 
 SITE_ID = 1
--- itproject/urls.py
+++ itproject/urls.py
@@ -30,6 +30,9 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 
 from mainapp.sitemaps import StaticViewSitemap, PostSitemap
+
+from django.views.generic.base import TemplateView #import TemplateView
+
 # from . import views
 
 sitemaps = {
@@ -57,6 +60,7 @@ urlpatterns = [
     path('', include('digitalmarketing.urls')),
     # path('robots.txt', include('robots.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),  #add the robots.txt file
 ]
 
 
